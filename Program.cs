657d8916f3b5c470745220d8ee5bcb1652483292
Sameer Sinha <sameersinha08@gmail.com>
2024-02-17 17:20:21 +0530

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,8 @@
+namespace ProGitForProgrammers {
+    class Program {
+        static void Main(String[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I just added this in Visual Studio");
+        }
+    }
+}
